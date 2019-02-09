431bcbe6a279739dc7c48acc222fbffa6299eb0c
Paul Rupek <7033911+pawelrupek@users.noreply.github.com>
2019-02-09 11:54:51 +0100

    Init mit vue

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import * as test from './modules/test.js'
-
-let x = (a, b) => a + b
-
-console.log(x(4,5))
-test.default()
-
--- src/modules/test.js
+++
@@ -1,6 +0,0 @@
-const addAndTurnToArray = (a, b) => [a, b, a+b]
-
-export default function () {
-    let x = addAndTurnToArray(3, 4)
-    console.log(...x)
-}
