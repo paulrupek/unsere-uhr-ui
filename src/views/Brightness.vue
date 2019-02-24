e08bfc14ba0df9d329e37592a62fb9c437851c9e
Paul Rupek <7033911+pawelrupek@users.noreply.github.com>
2019-02-24 15:17:33 +0100

    Helligkeit kann nun angepasst werden

--- src/views/Brightness.vue
+++ src/views/Brightness.vue
@@ -1,26 +1,88 @@
 <template>
     <div id="brightness container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Helligkeit</h1>
+            </div>
+            <div class="col-12">
+                <p>
+                    Hier kann konfiguriert werden wie die Uhr die aktuelle Helligkeit bestimmt.
+                </p>
+            </div>
 
+            <div class="col-6">
+                <uu-alert type="success" ref="successAlert">
+                    Die Einstellungen wurden erfolgreich angepasst.
+                </uu-alert>
+                <uu-alert type="error" ref="errorAlert">
+                    Ein Fehler ist beim Speichern aufgetreten.
+                </uu-alert>
+                <form action="#">
+                    <div class="form-group"><label for="strategy"></label>
+                        <label for="strategy">Strategie</label>
+                        <select id="strategy" class="form-control" v-model="data.strategy">
+                            <option v-for="(v, k) in data.strategies" :key="k">{{ v }}</option>
+                        </select>
+                    </div>
+                    <div v-if="data.strategy === 'FIXED_VALUE'" class="form-group">
+                        <label for="fixedValueStrategy">Helligkeit</label>
+                        <input id="fixedValueStrategy" class="form-control" v-model.number="data.fixedValue" type="number" min="0" max="100">
+                        <small id="fixedValueStrategyHelp" class="form-text text-muted">Bitte einen Helligkeitswert zwischen 0 und 100 erfassen.</small>
+                    </div>
 
-
-
-
-        
-        <h1 class="h1">Brightness settings</h1>
-        <pre>{{ JSON.stringify(data) }}</pre>
-        <h1>Strats</h1>
-        <div v-for="(v, k) in data" :key="k">
-            {{ v }} : {{ k }}
+                    <div v-if="data.strategy === 'SCHEDULE'" class="form-group">
+                        <div class="alert alert-warning" role="alert">
+                           Dieses Feature ist leider noch nicht implementiert.
+                        </div>
+                    </div>
+                    <button type="submit" @click="submitData" class="btn btn-primary">Speichern</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import Alert from '@/components/Alert.vue'
+
 export default {
+    components: {
+        'uu-alert': Alert
+    },
     data() {
         return {
             data: {},
 
+        }
+    },
+    methods: {
+        submitData(e) {
+            e.preventDefault()
+
+            // hide alerts, if present
+            this.$refs.successAlert.close()
+            this.$refs.errorAlert.close()
+
+            // PUT data
+            fetch('http://127.0.0.1:8081/settings/brightness', {
+                method: 'put',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.$data.data)
+            })
+            .then(r => r.json())
+            .then(r => {
+                if (r && r.success) {
+                    this.$refs.successAlert.open()
+                } else {
+                    this.$refs.errorAlert.open()
+                }
+            })
+            .catch(() => {
+                this.$refs.errorAlert.open()
+            })
         }
     },
     mounted() {
