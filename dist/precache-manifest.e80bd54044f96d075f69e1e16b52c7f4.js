8cdad7918ce5748a971ad808c0b917ad8d0c5b3a
Paul Rupek <7033911+pawelrupek@users.noreply.github.com>
2020-12-23 14:00:41 +0100

    Sauberer Build

---
+++ dist/precache-manifest.e80bd54044f96d075f69e1e16b52c7f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7be63d0dcbb17fdac7",
+    "url": "/css/app.d1e67661.css"
+  },
+  {
+    "revision": "62359ec329d4ccc72b429da898bee24e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c7be63d0dcbb17fdac7",
+    "url": "/js/app.f79a72af.js"
+  },
+  {
+    "revision": "35d67238dbf1b8c0e70a",
+    "url": "/js/chunk-vendors.46ad0107.js"
+  },
+  {
+    "revision": "6fb142ce4a5225cefb0b998d5096b2ec",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
